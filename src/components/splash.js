c88e0cd1bf319b50d33bcc0518ba49b72cf86405
Aceraney <ace.raney@gmail.com>
2018-11-18 16:12:16 -0500

    Update splash.js
    Updating Splash page.

--- src/components/splash.js
+++ src/components/splash.js
@@ -4,7 +4,7 @@ function splash () {
 		return(
 
 			<div className='splash-div'>
-			<h1 className = 'splash-header'> Front End Developer</h1>
+			<h1 className = 'splash-header'>Technolgist Specializing in Desktop Support</h1>
 			</div>
 	)
 	}
