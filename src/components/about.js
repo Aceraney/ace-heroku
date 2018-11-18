b03624b49a3003269cba5b637344ad0c024c6d55
Aceraney <ace.raney@gmail.com>
2018-11-18 16:10:23 -0500

    Update about.js
    Spelling mistake.

--- src/components/about.js
+++ src/components/about.js
@@ -18,7 +18,7 @@ function about () {
 				An up and coming front-end/full stack developer who enjoys learning and making things. This entire website has been coded using React and Redux.
 			</li>
 			<li className='about-item'>
-				Under the peojects tab, the Droese Raney Architecture tab is a clone I built of my father's website using React and Redux. 
+				Under the projects tab, the Droese Raney Architecture tab is a clone I built of my father's website using React and Redux. 
 			</li>
 			<li className='about-item'>
 				The Battlefield tab is a page designed to allow you to see your stats from the game Battlefield 4. If you do not play battlefield, the user "adam45" and "nxcyin" are usernames to try. 
