01465b379f465511dc45d5d7b5dd397e48ae1f54
Aceraney <ace.raney@gmail.com>
2020-03-10 11:21:42 -0400

    Update splash.js

--- src/components/splash.js
+++ src/components/splash.js
@@ -4,7 +4,7 @@ function splash () {
 		return(
 
 			<div className='splash-div'>
-			<h1 className = 'splash-header'>Technolgist Specializing in Desktop Support</h1>
+			<h1 className = 'splash-header'>Endpoint Automation Engineer at ASAPP</h1>
 			</div>
 	)
 	}
