26b78052e391f6ce81a20d7906eaeaa85fe809b8
Aceraney <ace.raney@gmail.com>
2020-03-10 11:23:21 -0400

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -11,20 +11,20 @@ function about () {
 			<ul>
 			<li className='about-item'>
 			
-				Currently working as a Systems Administrator, but has recently finished the classes equivilant to a B.S. in Computer Science.
+				Currently working as an Endpoint Automation Engineer at ASAPP.
 			
 			</li>
 			<li className='about-item'>
-				An up and coming front-end/full stack developer who enjoys learning and making things. This entire website has been coded using React and Redux.
+				 This entire website has been coded using React and Redux as a fun side project
 			</li>
 			<li className='about-item'>
-				Under the projects tab, the Droese Raney Architecture tab is a clone I built of my father's website using React and Redux. 
+				Under the projects tab, the Droese Raney Architecture tab is a clone I built of my father's website which has since been updated. 
 			</li>
 			<li className='about-item'>
 				The Battlefield tab is a page designed to allow you to see your stats from the game Battlefield 4. If you do not play battlefield, the user "adam45" and "nxcyin" are usernames to try. 
 			</li>
 			<li className='about-item'>
-				The Battlefield page uses the bf4stats.com api and uses the redux store and reducers to activly modify the state of the webpage. The "Save" button adds the user to the database and can be viewed again later. (WIP)
+				The Battlefield page uses the bf4stats.com api and uses the redux store and reducers to activly modify the state of the webpage. 
 			</li>
 			
 			</ul>
